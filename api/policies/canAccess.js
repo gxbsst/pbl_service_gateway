fea787e5ef511c315c51303c5339c10734287afd
liumin <alanliumin@gmail.com>
2014-11-24 14:51:45 +0800

    增加版本不符的错误返回信息

--- api/policies/canAccess.js
+++ api/policies/canAccess.js
@@ -1,7 +1,7 @@
 // 版本访问控制
 module.exports = function canAccess(req, res, next) {
   if (sails.config.version.enable && req.headers.accept !== sails.config.version.accept) {
-    return res.forbidden();
+    return res.forbidden('The requested version does not match.');
   }
   next();
 }
