3c3beae1804b37eac4acb40c665e4f3897b66f6d
liumin <alanliumin@gmail.com>
2014-11-25 21:47:42 +0800

    认证过程与DCI模型

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -5,10 +5,16 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var authentication = require('../contexts/authentication');
+
 module.exports = {
 
   index: function (req, res) {
     return res.fill(User.find());
+  },
+
+  authenticate: function(req, res) {
+    return res.fill(authentication().ask(req.body.id, req.body.password));
   }
 
 };
