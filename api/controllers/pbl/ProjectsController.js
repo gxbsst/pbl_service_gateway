64fdc93a1540c3f254cf157b9a027a688de55c28
liumin <alanliumin@gmail.com>
2014-12-18 15:49:35 +0800

    暂时注释掉project include

--- api/controllers/pbl/ProjectsController.js
+++ api/controllers/pbl/ProjectsController.js
@@ -2,47 +2,71 @@ var Promise = require("bluebird");
 
 module.exports = {
 
-  resource: 'Pbl.Project',
+  resource: 'Pbl.Project'
 
-  show: function (req, res) {
-    return res.fill(Pbl.Project.$$findOne({where: _.merge({_id: req.param('id')}, req.query)}).then(function (project) {
-
-      var findTechniques;
-      var findStandardItems;
-      var findRules;
-
-      if (project.techniques
-        && _.isArray(project.techniques)
-        && project.techniques.length > 0) {
-        findTechniques = Skill.Technique.$$find({where: {_id: project.techniques.join()}});
-      }
-
-      if (project.standard_items
-        && _.isArray(project.standard_items)
-        && project.standard_items.length > 0) {
-        findStandardItems = Curriculum.StandardItem.$$find({where: {_id: project.standard_items.join()}});
-      }
-
-      if (project.rules
-        && _.isArray(project.rules)
-        && project.rules.length > 0) {
-        findRules = Pbl.Rule.$$find({where: {_id: project.rules.join()}});
-      }
-
-      return Promise.join(findTechniques, findStandardItems, findRules, function (techniques, standard_items, rules) {
-        if (techniques) {
-          project.techniques = techniques;
-        }
-        if (standard_items) {
-          project.standard_items = standard_items;
-        }
-        if (rules) {
-          project.rules = rules;
-        }
-        return project;
-      });
-
-    }));
-  }
+  //show: function (req, res) {
+  //  return res.fill(Pbl.Project.$$findOne({where: _.merge({_id: req.param('id')}, req.query)}).then(function (project) {
+  //
+  //    var findTechniques;
+  //    var findStandardItems;
+  //    var findRules;
+  //
+  //    if (project.techniques
+  //      && _.isArray(project.techniques)
+  //      && project.techniques.length > 0) {
+  //
+  //      var techniqueIds = _.map(project.techniques, 'technique_id');
+  //      findTechniques = Skill.Technique.$$find({where: {_id: techniqueIds.join()}});
+  //    }
+  //
+  //    if (project.standard_items
+  //      && _.isArray(project.standard_items)
+  //      && project.standard_items.length > 0) {
+  //
+  //      var standardItemIds = _.map(project.standard_items, 'standard_item_id');
+  //      findStandardItems = Curriculum.StandardItem.$$find({where: {_id: standardItemIds.join()}});
+  //    }
+  //
+  //    if (project.rules
+  //      && _.isArray(project.rules)
+  //      && project.rules.length > 0) {
+  //
+  //      var ruleIds = _.map(project.rules, 'rule_id');
+  //      findRules = Pbl.Rule.$$find({where: {_id: ruleIds.join()}});
+  //    }
+  //
+  //    return Promise.join(findTechniques, findStandardItems, findRules, function (techniques, standard_items, rules) {
+  //      if (techniques) {
+  //        _.each(project.techniques, function(technique) {
+  //          _.each(techniques, function(t) {
+  //            if (technique.technique_id === t.id) {
+  //              technique.technique = t;
+  //            }
+  //          });
+  //        });
+  //      }
+  //      if (standard_items) {
+  //        _.each(project.standard_items, function(standard_item) {
+  //          _.each(standard_items, function(s){
+  //            if (standard_item.standard_item_id === s.id) {
+  //              standard_item.standard_item = s;
+  //            }
+  //          });
+  //        });
+  //      }
+  //      if (rules) {
+  //        _.each(project.rules, function(rule) {
+  //          _.each(rules, function(r) {
+  //            if (rule.rule_id === r.id) {
+  //              rule.rule = r;
+  //            }
+  //          });
+  //        });
+  //      }
+  //      return project;
+  //    });
+  //
+  //  }));
+  //}
 
 };
--- test/unit/controllers/pbl/ProjectsController.test.js
+++ test/unit/controllers/pbl/ProjectsController.test.js
@@ -33,222 +33,234 @@ describe('pbl/ProjectsController', function () {
 
   });
 
-  describe('#show()', function () {
-
-    before(function (done) {
-      nock("http://localhost:3000")
-        .get('/pbl/projects/1')
-        .reply(200, {
-          name: "Project",
-          description: "PBL Project"
-        });
-      done();
-    });
-
-    it('should respond a project', function (done) {
-      request(sails.hooks.http.app)
-        .get('/pbl/projects/1')
-        .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-          name: "Project",
-          description: "PBL Project"
-        }, done);
-    });
-
-    after(function (done) {
-      nock.cleanAll();
-      done();
-    });
-
-  });
-
-  describe('#show() - find project include techniques', function () {
-
-    before(function (done) {
-
-      nock("http://localhost:3000")
-        .get('/pbl/projects/1?include=techniques')
-        .reply(200, {
-          name: "Project",
-          description: "PBL Project",
-          techniques: ['1', '2', '3']
-        });
-
-      nock("http://localhost:3000")
-        .get('/skill/techniques/1,2,3')
-        .reply(200, [{name: "Technique 1"},
-          {name: "Technique 2"},
-          {name: "Technique 3"}]);
-
-      done();
-    });
-
-    it('should respond a project with techniques', function (done) {
-      request(sails.hooks.http.app)
-        .get('/pbl/projects/1?include=techniques')
-        .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-          name: "Project",
-          description: "PBL Project",
-          techniques: [
-            {name: "Technique 1"},
-            {name: "Technique 2"},
-            {name: "Technique 3"}
-          ]
-        }, done);
-    });
-
-    after(function (done) {
-      nock.cleanAll();
-      done();
-    });
-
-  });
-
-  describe('#show() - find project include standard_items', function () {
-
-    before(function (done) {
-
-      nock("http://localhost:3000")
-        .get('/pbl/projects/1?include=standard_items')
-        .reply(200, {
-          name: "Project",
-          description: "PBL Project",
-          standard_items: ['1', '2', '3']
-        });
-
-      nock("http://localhost:3000")
-        .get('/curriculum/standard_items/1,2,3')
-        .reply(200, [{name: "Item 1"},
-          {name: "Item 2"},
-          {name: "Item 3"}]);
-
-      done();
-    });
-
-    it('should respond a project with techniques', function (done) {
-      request(sails.hooks.http.app)
-        .get('/pbl/projects/1?include=standard_items')
-        .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-          name: "Project",
-          description: "PBL Project",
-          standard_items: [
-            {name: "Item 1"},
-            {name: "Item 2"},
-            {name: "Item 3"}
-          ]
-        }, done);
-    });
-
-    after(function (done) {
-      nock.cleanAll();
-      done();
-    });
-
-  });
-
-  describe('#show() - find project include rules', function () {
-
-    before(function (done) {
-
-      nock("http://localhost:3000")
-        .get('/pbl/projects/1?include=rules')
-        .reply(200, {
-          name: "Project",
-          description: "PBL Project",
-          rules: ['1', '2', '3']
-        });
-
-      nock("http://localhost:3000")
-        .get('/pbl/rules/1,2,3')
-        .reply(200, [{name: "Rule 1"},
-          {name: "Rule 2"},
-          {name: "Rule 3"}]);
-
-      done();
-    });
-
-    it('should respond a project with rules', function (done) {
-      request(sails.hooks.http.app)
-        .get('/pbl/projects/1?include=rules')
-        .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-          name: "Project",
-          description: "PBL Project",
-          rules: [
-            {name: "Rule 1"},
-            {name: "Rule 2"},
-            {name: "Rule 3"}
-          ]
-        }, done);
-    });
-
-    after(function (done) {
-      nock.cleanAll();
-      done();
-    });
-
-  });
-
-  describe('#show() - find project include standard_items & rules', function () {
-
-    before(function (done) {
-
-      nock("http://localhost:3000")
-        .get('/pbl/projects/1?include=standard_items%2Crules')
-        .reply(200, {
-          name: "Project",
-          description: "PBL Project",
-          standard_items: ['1', '2', '3'],
-          rules: ['1', '2', '3']
-        });
-
-      nock("http://localhost:3000")
-        .get('/curriculum/standard_items/1,2,3')
-        .reply(200, [{name: "Item 1"},
-          {name: "Item 2"},
-          {name: "Item 3"}]);
-
-      nock("http://localhost:3000")
-        .get('/pbl/rules/1,2,3')
-        .reply(200, [{name: "Rule 1"},
-          {name: "Rule 2"},
-          {name: "Rule 3"}]);
-
-      done();
-    });
-
-    it('should respond a project with rules', function (done) {
-      request(sails.hooks.http.app)
-        .get('/pbl/projects/1?include=standard_items,rules')
-        .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-          name: "Project",
-          description: "PBL Project",
-          standard_items: [
-            {name: "Item 1"},
-            {name: "Item 2"},
-            {name: "Item 3"}
-          ],
-          rules: [
-            {name: "Rule 1"},
-            {name: "Rule 2"},
-            {name: "Rule 3"}
-          ]
-        }, done);
-    });
-
-    after(function (done) {
-      nock.cleanAll();
-      done();
-    });
-
-  });
+  //describe('#show()', function () {
+  //
+  //  before(function (done) {
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/projects/1')
+  //      .reply(200, {
+  //        name: "Project",
+  //        description: "PBL Project"
+  //      });
+  //    done();
+  //  });
+  //
+  //  it('should respond a project', function (done) {
+  //    request(sails.hooks.http.app)
+  //      .get('/pbl/projects/1')
+  //      .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
+  //      .expect('Content-Type', /json/)
+  //      .expect(200, {
+  //        name: "Project",
+  //        description: "PBL Project"
+  //      }, done);
+  //  });
+  //
+  //  after(function (done) {
+  //    nock.cleanAll();
+  //    done();
+  //  });
+  //
+  //});
+  //
+  //describe('#show() - find project include techniques', function () {
+  //
+  //  before(function (done) {
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/projects/1?include=techniques')
+  //      .reply(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        techniques: [
+  //          { id: 'project_technique_id_1', technique_id: 'technique_id_1' },
+  //          { id: 'project_technique_id_2', technique_id: 'technique_id_2' },
+  //          { id: 'project_technique_id_3', technique_id: 'technique_id_3' }
+  //        ]
+  //      });
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/skill/techniques/technique_id_1,technique_id_2,technique_id_3')
+  //      .reply(200, [
+  //        { id: 'technique_id_1', name: "Technique 1" },
+  //        { id: 'technique_id_2', name: "Technique 2" },
+  //        { id: 'technique_id_3', name: "Technique 3" }
+  //      ]);
+  //
+  //    done();
+  //  });
+  //
+  //  it('should respond a project with techniques', function (done) {
+  //    request(sails.hooks.http.app)
+  //      .get('/pbl/projects/1?include=techniques')
+  //      .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
+  //      .expect('Content-Type', /json/)
+  //      .expect(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        techniques: [
+  //          { id: 'project_technique_id_1', technique_id: 'technique_id_1', technique: { id: 'technique_id_1', name: "Technique 1" } },
+  //          { id: 'project_technique_id_2', technique_id: 'technique_id_2', technique: { id: 'technique_id_2', name: "Technique 2" } },
+  //          { id: 'project_technique_id_3', technique_id: 'technique_id_3', technique: { id: 'technique_id_3', name: "Technique 3" } }
+  //        ]
+  //      }, done);
+  //  });
+  //
+  //  after(function (done) {
+  //    nock.cleanAll();
+  //    done();
+  //  });
+  //
+  //});
+  //
+  //describe('#show() - find project include standard_items', function () {
+  //
+  //  before(function (done) {
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/projects/1?include=standard_items')
+  //      .reply(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        standard_items: [
+  //          { id: 'project_si_id_1', standard_item_id: 'si_id_1' },
+  //          { id: 'project_si_id_2', standard_item_id: 'si_id_2' },
+  //          { id: 'project_si_id_3', standard_item_id: 'si_id_3' }
+  //        ]
+  //      });
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/curriculum/standard_items/si_id_1,si_id_2,si_id_2')
+  //      .reply(200, [
+  //        { id: 'si_id_1', name: "Item 1" },
+  //        { id: 'si_id_1', name: "Item 2" },
+  //        { id: 'si_id_3', name: "Item 3"}
+  //      ]);
+  //
+  //    done();
+  //  });
+  //
+  //  it('should respond a project with standard_items', function (done) {
+  //    request(sails.hooks.http.app)
+  //      .get('/pbl/projects/1?include=standard_items')
+  //      .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
+  //      .expect('Content-Type', /json/)
+  //      .expect(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        standard_items: [
+  //          { id: 'project_si_id_1', standard_item_id: 'si_id_1', standard_item: { id: 'si_id_1', name: "Item 1" } },
+  //          { id: 'project_si_id_2', standard_item_id: 'si_id_2', standard_item: { id: 'si_id_2', name: "Item 2" } },
+  //          { id: 'project_si_id_3', standard_item_id: 'si_id_3', standard_item: { id: 'si_id_3', name: "Item 3" } }
+  //        ]
+  //      }, done);
+  //  });
+  //
+  //  after(function (done) {
+  //    nock.cleanAll();
+  //    done();
+  //  });
+  //
+  //});
+  //
+  //describe('#show() - find project include rules', function () {
+  //
+  //  before(function (done) {
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/projects/1?include=rules')
+  //      .reply(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        rules: ['1', '2', '3']
+  //      });
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/rules/1,2,3')
+  //      .reply(200, [{name: "Rule 1"},
+  //        {name: "Rule 2"},
+  //        {name: "Rule 3"}]);
+  //
+  //    done();
+  //  });
+  //
+  //  it('should respond a project with rules', function (done) {
+  //    request(sails.hooks.http.app)
+  //      .get('/pbl/projects/1?include=rules')
+  //      .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
+  //      .expect('Content-Type', /json/)
+  //      .expect(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        rules: [
+  //          {name: "Rule 1"},
+  //          {name: "Rule 2"},
+  //          {name: "Rule 3"}
+  //        ]
+  //      }, done);
+  //  });
+  //
+  //  after(function (done) {
+  //    nock.cleanAll();
+  //    done();
+  //  });
+  //
+  //});
+  //
+  //describe('#show() - find project include standard_items & rules', function () {
+  //
+  //  before(function (done) {
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/projects/1?include=standard_items%2Crules')
+  //      .reply(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        standard_items: ['1', '2', '3'],
+  //        rules: ['1', '2', '3']
+  //      });
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/curriculum/standard_items/1,2,3')
+  //      .reply(200, [{name: "Item 1"},
+  //        {name: "Item 2"},
+  //        {name: "Item 3"}]);
+  //
+  //    nock("http://localhost:3000")
+  //      .get('/pbl/rules/1,2,3')
+  //      .reply(200, [{name: "Rule 1"},
+  //        {name: "Rule 2"},
+  //        {name: "Rule 3"}]);
+  //
+  //    done();
+  //  });
+  //
+  //  it('should respond a project with rules', function (done) {
+  //    request(sails.hooks.http.app)
+  //      .get('/pbl/projects/1?include=standard_items,rules')
+  //      .set('Accept', 'application/vnd.ibridgebrige.com; version=1')
+  //      .expect('Content-Type', /json/)
+  //      .expect(200, {
+  //        name: "Project",
+  //        description: "PBL Project",
+  //        standard_items: [
+  //          {name: "Item 1"},
+  //          {name: "Item 2"},
+  //          {name: "Item 3"}
+  //        ],
+  //        rules: [
+  //          {name: "Rule 1"},
+  //          {name: "Rule 2"},
+  //          {name: "Rule 3"}
+  //        ]
+  //      }, done);
+  //  });
+  //
+  //  after(function (done) {
+  //    nock.cleanAll();
+  //    done();
+  //  });
+  //
+  //});
 
 });
