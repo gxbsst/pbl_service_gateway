403be1f1234b07b9cbce2e2f2dac224f18e63fa6
liumin <alanliumin@gmail.com>
2014-12-21 12:23:33 +0800

    products include resources

--- api/controllers/pbl/ProductsController.js
+++ api/controllers/pbl/ProductsController.js
@@ -8,6 +8,11 @@ module.exports = {
         param: 'product_forms',
         model: 'ProductForm',
         via: 'product_form_id'
+      },
+      {
+        param: 'resources',
+        model: 'Resource',
+        via: 'resource_id'
       }
     ]
   }
