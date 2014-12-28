56b1844b885d789a2a0bf3df4cbf04ac256d8f5a
liumin <alanliumin@gmail.com>
2014-12-28 19:27:50 +0800

    follows include users

--- api/controllers/FollowsController.js
+++ api/controllers/FollowsController.js
@@ -1,5 +1,15 @@
 module.exports = {
 
-  resource: 'Follow'
+  resource: 'Follow',
+
+  include: {
+    index: [
+      {
+        param: 'users',
+        model: 'User',
+        via: 'user_id'
+      }
+    ]
+  }
 
 };
