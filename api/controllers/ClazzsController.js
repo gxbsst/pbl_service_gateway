45ceaa4f0603d6349b374b3ee52eaa43e7fbb965
gxbsst <gxbsst@gmail.com>
2015-01-19 11:04:06 +0800

    增加 Invitation\School\Grade\Clazz.

---
+++ api/controllers/ClazzsController.js
@@ -0,0 +1,14 @@
+/**
+ * ClazzsController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  index: function (req, res) {
+    return res.ok();
+  }
+};
+
---
+++ api/controllers/SchoolsController.js
@@ -0,0 +1,14 @@
+/**
+ * SchoolsController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  index: function (req, res) {
+    return res.ok();
+  }
+};
+
