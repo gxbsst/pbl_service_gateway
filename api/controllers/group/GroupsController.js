ff8ad53bcfa8b1ca67b71ec714a2e6fb39b4a6cc
liao <aiqing0342@qq.com>
2015-01-29 17:38:56 +0800

    。。。

--- api/controllers/group/GroupsController.js
+++ api/controllers/group/GroupsController.js
@@ -2,6 +2,17 @@ module.exports = {
 
   resource: 'Group.Group',
 
+  include: {
+      show: [
+        {
+          param: 'clazzs',
+          model: 'Clazz',
+          via: 'owner_id',
+          embed: 'clazz'
+        }
+      ]
+  },
+
   join: [
     {
       model: 'Invitation',
