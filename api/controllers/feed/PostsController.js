cc71b30bee14f1dc632c1afbdfd95b67cf906ae1
liao <aiqing0342@qq.com>
2015-01-31 16:08:28 +0800

    include sender

--- api/controllers/feed/PostsController.js
+++ api/controllers/feed/PostsController.js
@@ -1,5 +1,24 @@
 module.exports = {
 
-  resource: 'Feed.Post'
+  resource: 'Feed.Post',
+
+  include: {
+    index: [
+      {
+        param: 'sender',
+        model: 'User',
+        via: 'sender_id',
+        embed: 'sender'
+      }
+    ],
+    show: [
+      {
+        param: 'sender',
+        model: 'User',
+        via: 'sender_id',
+        embed: 'sender'
+      }
+    ]
+  }
 
 };
